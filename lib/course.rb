e2c484caf577b066e9ffa77d94852fb69046d388
Alicia Santiago <albrown07@mac.com>
2019-03-05 21:33:28 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,11 +2,11 @@ class Course
 attr_accessor :title, :schedule, :description
 @@all = []
 
-  def initialize(title, schedule, description)
-    @@all << @title
-    @@all << @schedule
-    @@all << @description
-  end
+  # def initialize(title, schedule, description)
+  #   @@all << @title
+  #   @@all << @schedule
+  #   @@all << @description
+  # end
 
   def self.all
     @@all
